a87eed97dcde465455ede0f0de92f96b37bb38d7
cesarecaoduro <cesare.caoduro@gmail.com>
2026-03-16 21:38:10 +1100

    Fix web binding runtime augment

--- bindings/web/src/index.ts
+++ bindings/web/src/index.ts
@@ -1,25 +1,5 @@
-import { KernelSession } from "../../../crates/kernel/pkg/rusted_geom.js";
-import "./kernel-augment.js";
-// wasm-bindgen generated classes (KernelSession, CurveHandle, SurfaceHandle, …).
+import "./runtime-augment.js";
+
 export * from "../../../crates/kernel/pkg/rusted_geom.js";
-
-if (!(KernelSession.prototype as { export_usdc?: unknown }).export_usdc) {
-  Object.defineProperties(KernelSession.prototype, {
-    export_usdc: {
-      value(this: KernelSession, objectIds?: Float64Array, endpoint?: string) {
-        return import("./export-runtime.js").then((mod) => mod.exportUsdcBinary(this, objectIds, endpoint));
-      },
-    },
-    export_glb: {
-      value(this: KernelSession, objectIds: Float64Array) {
-        return import("./export-runtime.js").then((mod) => mod.exportGlbBinary(this, objectIds));
-      },
-    },
-  });
-}
-
-// USD value/schema interfaces and compatibility aliases.
 export * from "./types";
-
-// Convenience WASM loader.
 export { loadKernel } from "./loader";
